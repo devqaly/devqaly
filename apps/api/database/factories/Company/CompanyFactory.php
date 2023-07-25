01c305d72dfdfa8d674a4b0fb82e0831e151dca2
Bruno Francisco <bruno.francisco@codotto.com>
2023-07-25 20:41:21 +0200

    - added tests to `CompanyMemberController`

--- apps/api/database/factories/Company/CompanyFactory.php
+++ apps/api/database/factories/Company/CompanyFactory.php
@@ -2,10 +2,13 @@
 
 namespace Database\Factories\Company;
 
+use App\Models\Company\Company;
+use App\Models\Company\CompanyMember;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Company\Company>
+ * @extends Factory<Company>
  */
 class CompanyFactory extends Factory
 {
@@ -17,7 +20,38 @@ class CompanyFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->company(),
+            'created_by_id' => User::factory(),
         ];
+    }
+
+    public function configure()
+    {
+        return $this->afterCreating(function (Company $company) {
+            CompanyMember::create([
+                'company_id' => $company->id,
+                'member_id' => $company->created_by_id,
+                // Currently we do not care about the register_token_id
+                'register_token_id' => null,
+                'invited_by_id' => $company->created_by_id,
+            ]);
+        });
+    }
+
+    public function withMembers($count = 5): CompanyFactory
+    {
+        return $this->afterCreating(function (Company $company) use ($count) {
+            $users = User::factory()->count($count)->create();
+
+            $users->each(function (User $user) use ($company) {
+                CompanyMember::create([
+                    'company_id' => $company->id,
+                    'member_id' => $user->id,
+                    // Currently we do not care about the register_token_id
+                    'register_token_id' => null,
+                    'invited_by_id' => $company->created_by_id,
+                ]);
+            });
+        });
     }
 }
--- apps/api/routes/api/resources/company.php
+++ apps/api/routes/api/resources/company.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\Resources\CompanyMemberController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('companies/{company}/members', [CompanyMemberController::class, 'index']);
-    Route::post('companies/{company}/members', [CompanyMemberController::class, 'store']);
+    Route::get('companies/{company}/members', [CompanyMemberController::class, 'index'])->name('companyMembers.index');
+    Route::post('companies/{company}/members', [CompanyMemberController::class, 'store'])->name('companyMembers.store');
 });
