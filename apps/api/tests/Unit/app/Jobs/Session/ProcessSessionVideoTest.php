39df6743494d8cda071d2304b06fdfa0e1226748
Bruno Francisco <bruno.francisco@codotto.com>
2023-08-04 17:02:37 +0200

    Add test to `app/Jobs/Session/ProcessSessionVideo.php`
    - added test to test `ProcessSessionVideo`
    - changed visibility of methods from `private` to `protected` in `app/Jobs/Session/ProcessSessionVideo.php`
    	- I thought it would be easier to change it to protected instead of mocking the entirety of `Storage`
    	  if we would go down this road, the test would be harder to read
    - added a new `test-video.webm` under `storage/app/videos-to-convert` to use this to be able to test things
    - closes #23

---
+++ apps/api/tests/Unit/app/Jobs/Session/ProcessSessionVideoTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Tests\Unit\app\Jobs\Session;
+
+use App\Enum\Sessions\SessionVideoStatusEnum;
+use App\Jobs\Session\ProcessSessionVideo;
+use App\Models\Session\Session;
+use Illuminate\Support\Facades\Storage;
+use Mockery;
+use Tests\TestCase;
+
+class ProcessSessionVideoTest extends TestCase
+{
+    public function test_video_gets_processed(): void
+    {
+        $session = Session::factory()->create();
+
+        $job = new ProcessSessionVideo($session);
+
+        // We can't fake the disk here because we will be directly
+        // calling `shell_exec` from within `ProcessSessionVideo::handle` method
+        Storage::disk('local')
+            ->copy(
+                "videos-to-convert/test-video.webm",
+                "videos-to-convert/{$session->id}.{$session->video_extension}"
+            );
+
+        $job->handle();
+
+        $session->refresh();
+
+        $this->assertEquals(32, $session->video_duration_in_seconds);
+        $this->assertEquals(2.065318107605, $session->video_size_in_megabytes);
+        $this->assertEquals(100, $session->video_conversion_percentage);
+        $this->assertEquals(SessionVideoStatusEnum::CONVERTED, $session->video_status);
+        $this->assertNotNull($session->started_video_conversion_at);
+        $this->assertNotNull($session->ended_video_conversion_at);
+
+        Storage::assertExists("videos/{$session->id}.webm");
+        Storage::delete("videos/{$session->id}.webm");
+    }
+
+    public function test_should_download_file_first_if_using_s3_filesystem(): void
+    {
+        $session = Session::factory()->create();
+
+        $processVideoMock = Mockery::mock(ProcessSessionVideo::class, [$session])
+            ->makePartial();
+
+        $processVideoMock
+            ->shouldAllowMockingProtectedMethods()
+            ->shouldReceive('isUsingS3Bucket')
+            ->zeroOrMoreTimes()
+            ->andReturn(true);
+
+        $processVideoMock
+            ->shouldAllowMockingProtectedMethods()
+            ->shouldReceive('downloadFileFromS3')
+            ->once();
+
+        $processVideoMock
+            ->shouldAllowMockingProtectedMethods()
+            ->shouldReceive('uploadFileToS3')
+            ->once();
+
+        // We can't fake the disk here because we will be directly
+        // calling `shell_exec` from within `ProcessSessionVideo::handle` method
+        Storage::disk('local')
+            ->copy(
+                "videos-to-convert/test-video.webm",
+                "videos-to-convert/{$session->id}.{$session->video_extension}"
+            );
+
+        $processVideoMock->handle();
+
+        $session->refresh();
+
+        $this->assertEquals(100, $session->video_conversion_percentage);
+        $this->assertEquals(SessionVideoStatusEnum::CONVERTED, $session->video_status);
+        $this->assertNotNull($session->started_video_conversion_at);
+        $this->assertNotNull($session->ended_video_conversion_at);
+
+        Storage::assertExists("videos/{$session->id}.webm");
+        Storage::delete("videos/{$session->id}.webm");
+    }
+}
