3606496ed802310a171f4b6f5d70dff439e4d19d
Bruno Francisco <bruno.francisco@codotto.com>
2023-09-07 15:53:54 +0100

    - returning subscription for company

--- apps/api/app/Http/Resources/Resources/CompanyResource.php
+++ apps/api/app/Http/Resources/Resources/CompanyResource.php
@@ -4,6 +4,8 @@ namespace App\Http\Resources\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Collection;
+use Laravel\Cashier\SubscriptionItem;
 
 class CompanyResource extends JsonResource
 {
@@ -14,10 +16,22 @@ class CompanyResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [
+        return collect([
             'id' => $this->id,
             'name' => $this->name,
             'createdAt' => $this->created_at,
-        ];
+            'subscription' => $this->subscription(),
+        ])
+            ->when(!config('devqaly.isSelfHosting'), function (Collection $collection) {
+                $subscription = $this->subscription();
+
+                $collection->put('subscription', $subscription ? [
+                    'endsAt' => $subscription->ends_at,
+                    'createdAt' => $subscription->created_at,
+                    'status' => $subscription->stripe_status,
+                    'trialEndsAt' => $subscription->trial_ends_at,
+                ] : null);
+            })
+            ->toArray();
     }
 }
