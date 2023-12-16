51a9d4aae876f8d5e3669afd8b148364f5c23b7d
Bruno Francisco <bruno.francisco@codotto.com>
2023-12-16 13:13:59 +0100

    - removed `primeflex` from project

--- apps/webapp/src/main.ts
+++ apps/webapp/src/main.ts
@@ -1,6 +1,5 @@
 import 'primevue/resources/themes/lara-light-blue/theme.css'
 import 'primevue/resources/primevue.min.css'
-import 'primeflex/primeflex.css'
 import 'primeicons/primeicons.css'
 import './global.css'
 
