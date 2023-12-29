76f4c92eaea004da8feda8526f8d6bdde09ec3a9
Bruno Francisco <bruno.francisco@codotto.com>
2023-12-29 11:05:19 +0100

    - added skeleton to create screen where user sees video

---
+++ apps/webapp/src/components/pages/onboarding/SeeSessionOnboardingDialog.vue
@@ -0,0 +1,46 @@
+<template>
+  <Dialog
+    ref="dialog"
+    :pt="{
+      root: { class: 'p-dialog-maximized' }
+    }"
+    :visible="visible"
+    :draggable="false"
+    modal
+    header="See your first session"
+    @update:visible="onHide"
+  >
+    <div class="grid grid-cols-12 gap-5">
+      <div class="col-span-2"></div>
+
+      <div class="col-span-7"></div>
+
+      <div class="col-span-3"></div>
+    </div>
+  </Dialog>
+</template>
+
+<script setup lang="ts">
+import { PropType, ref } from 'vue'
+import Dialog from 'primevue/dialog'
+import type { SessionCodec } from '@/services/api/resources/session/codec'
+
+const dialog = ref<InstanceType<typeof Dialog> | null>()
+
+defineProps({
+  visible: {
+    type: Boolean,
+    required: true
+  },
+  session: {
+    type: Object as PropType<SessionCodec>,
+    required: true
+  }
+})
+
+const emit = defineEmits(['hide'])
+
+function onHide(value: boolean) {
+  emit('hide', value)
+}
+</script>
--- apps/webapp/src/views/onboarding/OnboardingCreateSessionView.vue
+++ apps/webapp/src/views/onboarding/OnboardingCreateSessionView.vue
@@ -11,11 +11,17 @@
       </a>
     </div>
 
+    <SeeSessionOnboardingDialog
+      :visible="isDialogOpen"
+      @hide="(value: boolean) => isDialogOpen = value"
+    />
+
     <div class="mt-2 px-5 pt-2 pb-4">
       Now that you have installed Devqaly's SDK, you can create your first session.
 
       <div
         class="bg-slate-100 w-full aspect-video flex justify-center items-center rounded-lg mt-2 text-xl"
+        @click="isDialogOpen = true"
       >
         Your session will show up here when we receive it
       </div>
@@ -36,6 +42,46 @@
 
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
+import { onMounted, ref } from 'vue'
+import SeeSessionOnboardingDialog from '@/components/pages/onboarding/SeeSessionOnboardingDialog.vue'
+import { emptyPagination, OrderBy, PaginatableRecord } from '@/services/api'
+import type { SessionCodec } from '@/services/api/resources/session/codec'
+import { useToast } from 'primevue/usetoast'
+import { getProjectSessions } from '@/services/api/resources/project/actions'
 
 const route = useRoute()
+
+const isDialogOpen = ref(false)
+
+const sessionsResponse = ref(emptyPagination<SessionCodec>())
+
+const toast = useToast()
+
+let fetchSessionsClear: ReturnType<typeof setInterval>
+
+async function fetchSessions() {
+  try {
+    const response = await getProjectSessions(route.params.projectId as string, {
+      createdAtOrder: OrderBy.DESC
+    })
+
+    sessionsResponse.value = response.data as PaginatableRecord<SessionCodec>
+
+    if (response.data.meta.total > 0) {
+      clearInterval(fetchSessionsClear)
+    }
+  } catch (e) {
+    toast.add({
+      severity: 'error',
+      summary: 'Error',
+      detail: 'Error fetching details about sessions',
+      life: 3000,
+      group: 'bottom-center'
+    })
+  }
+}
+
+onMounted(() => {
+  fetchSessionsClear = setInterval(fetchSessions, 2000)
+})
 </script>
